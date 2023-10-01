0f3c3a7aa7d5ab9fabbd7616add25e03ff4f484b
AlexGosselin <alexandergosselin@outlook.com>
2023-09-30 20:35:17 -0400

    Created Initial Project

---
+++ SpendWise/SpendWise/SpendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendWiseApp.swift
+//  SpendWise
+//
+//  Created by Alexander Gosselin on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct SpendWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
