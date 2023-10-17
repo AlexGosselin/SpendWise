31679b098d90cb134cdf6038a93b4e94d2bb496f
AlexGosselin <alexandergosselin@outlook.com>
2023-10-16 21:20:52 -0400

    [#1] Implemented basic ui for add expense screen

--- SpendWise/SpendWise/Components/AddExpense.swift
+++ SpendWise/SpendWise/Components/AddExpense.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct AddExpense: View {
+    
+    @State var title = ""
+    @State var amount = 0.0
+    @State var date = Date()
+    @State var description = ""
+    @State var category = ""
+    
+    var categories = ["cat1", "cat2", "cat3hgdlhdjfsla"]
+    
+    var currencyFormatter = NumberFormatter()
+    
+    init() {
+        self.currencyFormatter.numberStyle = NumberFormatter.Style.currency
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Add Expense")
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            HStack {
+                Text("Title")
+                Spacer()
+                TextField("My Expense", text: $title)
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding()
+
+            HStack {
+                Text("Amount")
+                Spacer()
+                TextField("Amount", value: $amount, formatter: currencyFormatter)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding()
+            
+            HStack {
+                Text("Category")
+                Spacer()
+                Picker("", selection: $category) {
+                    ForEach(categories, id: \.self) { cat in
+                        Text(cat)
+                    }
+                }
+                .pickerStyle(.menu)
+                .cornerRadius(10.0)
+                .background(RoundedRectangle(cornerRadius: 10.0).fill(.quinary))
+            }
+            .padding()
+            
+
+            DatePicker("Date", selection: $date, displayedComponents: [.date])
+                .padding()
+            
+            VStack(alignment: .leading) {
+                Text("Description")
+                TextEditor(text: $description)
+                    .cornerRadius(10.0)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(.quaternary, lineWidth: 2))
+            }
+            .padding()
+            
+            Button("Save") {
+                //save expense here
+            }
+            
+            Spacer()
+        }
+        .padding()
+
     }
 }
 
