b5a21947f6aaf3d3230f1d2c86b98fe5fcd34e3c
AlexGosselin <alexandergosselin@outlook.com>
2023-11-05 18:50:35 -0500

    [#10] Added alerts to the delete buttons in the settings screen

--- SpendWise/SpendWise/Views/SettingsView.swift
+++ SpendWise/SpendWise/Views/SettingsView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+//    @Environment(CategoryViewModel.self) private var categoryViewModel: CategoryViewModel
+//    @EnvironmentObject var expenseViewModel: ExpenseViewModel
+    
+    @State private var showExpenseAlert = false
+    @State private var showCategoryAlert = false
+    @State private var showDataAlert = false
+    
     var body: some View {
         
         ZStack {
@@ -16,15 +24,39 @@ struct SettingsView: View {
                 List {
                     Section(header: Text("Data")) {
                         Button("Delete All Expenses", role: .destructive) {
-                            
+                            showExpenseAlert = true
+                        }
+                        .alert("Delete all expenses?", isPresented: $showExpenseAlert) {
+                            Button("Cancel", role: .cancel){}
+                            Button("Delete", role: .destructive){
+                                
+                            }
+                        } message: {
+                            Text("This cannot be undone!")
                         }
                         
                         Button("Delete All User Created Categories", role: .destructive) {
-                            
+                            showCategoryAlert = true
+                        }
+                        .alert("Delete all user created categories?", isPresented: $showCategoryAlert) {
+                            Button("Cancel", role: .cancel){}
+                            Button("Delete", role: .destructive){
+                                
+                            }
+                        } message: {
+                            Text("This cannot be undone!")
                         }
                         
                         Button("Delete All User Data", role: .destructive) {
-                            
+                            showDataAlert = true
+                        }
+                        .alert("Delete all user data?", isPresented: $showDataAlert) {
+                            Button("Cancel", role: .cancel){}
+                            Button("Delete", role: .destructive){
+                                
+                            }
+                        } message: {
+                            Text("This cannot be undone!")
                         }
                     }
                 }
