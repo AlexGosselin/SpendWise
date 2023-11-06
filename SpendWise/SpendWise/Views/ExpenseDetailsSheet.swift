9c57f10eab4650348c6ec6f2ad40ddc9ba678dc4
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-11-06 13:30:38 -0500

    [#5] Passing new variables to be displayed on the expenses sheet
    Adding expenses title, transaction type to the view expenses sheet. Also passing the correct value for category name.

--- SpendWise/SpendWise/Views/ExpenseDetailsSheet.swift
+++ SpendWise/SpendWise/Views/ExpenseDetailsSheet.swift
@@ -48,9 +48,11 @@ struct ExpenseDetailsSheet: View {
                     
                     Divider()
                     
+                    DetailRows(title: "Title", text: "\(expenses.title)")
                     DetailRows(title: "Amount", text: "$ \(expenses.amount)")
-                    DetailRows(title: "Date & Time", text: "\(expenses.dateParsed)")
-                    DetailRows(title: "Category", text: expenses.categoryItem.name)
+                    DetailRows(title: "Transaction Type", text: "\(expenses.transactionType)")
+                    DetailRows(title: "Date & Time", text: "\(expenses.dateString)")
+                    DetailRows(title: "Category", text: expenses.category?.name ?? "-")
                     DetailRows(title: "Description", text: "\(expenses.desc ?? "-")")
                     
                     Spacer()
@@ -82,7 +84,7 @@ struct DetailRows: View {
         HStack {
             Text("\(title):")
                 .foregroundStyle(.gray)
-                .frame(maxWidth: 120, alignment: .leading)
+                .frame(maxWidth: 145, alignment: .leading)
             
             Text(text)
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
